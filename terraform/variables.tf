8c4849cdc91a88e6f09edc0362907536d0742818
GAURAV LODHI <gaurav@GAURAV-ka-MacBook-Air.local>
2025-07-20 15:24:11 +0530

    minor updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,23 @@
-variable "aws_region" {
-  description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
-}
-
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  default     = "t2.medium"
-}
+  description = "Type of the EC2 instance"
+  type        = string
+  default     = "t3.medium"
 
-variable "my_enviroment" {
-  description = "Instance type for the EC2 instance"
-  default     = "dev"
+}
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+variable "root_volume_size" {
+  description = "Size of the root volume in GB"
+  type        = number
+  default     = 30
+
+}
+variable "env" {
+  description = "Environment for the deployment (e.g., dev, prod)"
+  type        = string
+  default     = "prod"
+
 }
